4c728370785c217d1c6832c1135a4317aa34f16b
PhucTran <tranhieuphuc12@gmail.com>
2023-12-24 12:34:28 +0700

    restore index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config/database.php';
+
+$page = 1;
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+$perPage = 6;
+
+$template = new Template();
+$productModel = new Product();
+$products = $productModel-> getProductsByPage($page, $perPage);
+$total = $productModel-> getTotalProducts();
+ 
+// Đổ dữ liệu vào blocks
+$slot = $template->render('product_list_block', ['products' => $products,
+                                                'perPage' => $perPage,
+                                                'total' => $total,
+                                                'page' =>$page
+                                            ]);
+
+// Chuẩn bị dữ liệu gồm các blocks có data đã được đổ vào block
+$data = [
+    'title' => 'Home',
+    'slot' => $slot
+];
+// Gọi layout vào đây với dữ liệu đã làm ở trên
+$template->view('navbar_dark_layout', $data);
